481f506efbfd63bb412d0dfdab860632fa90f737
ma5ter <ma5ter@mail>
2026-08-25 10:33:49 +0300

    add github release automation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,22 @@ android {
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
+	signingConfigs {
+		create("release") {
+			val keystoreFile = file("release.keystore")
+			if (keystoreFile.exists()) {
+				storeFile = keystoreFile
+				storePassword = System.getenv("KEYSTORE_PASSWORD")
+				keyAlias = System.getenv("KEY_ALIAS")
+				keyPassword = System.getenv("KEY_PASSWORD")
+			}
+		}
+	}
+
 	buildTypes {
 		release {
 			isMinifyEnabled = false
+			signingConfig = signingConfigs.getByName("release")
 			proguardFiles(
 				getDefaultProguardFile("proguard-android-optimize.txt"),
 				"proguard-rules.pro"
