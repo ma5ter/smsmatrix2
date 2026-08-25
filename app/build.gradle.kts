5da6fdcc4a7781e3ff13d9f710365c2095e9610c
ma5ter <ma5ter@mail>
2026-08-25 12:00:51 +0300

    add f-droid metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,14 @@ android {
 		applicationId = "su.mya.sms_matrix"
 		minSdk = 29
 		targetSdk = 37
-		versionCode = 1
-		versionName = "1.0"
+		versionCode = 10
+		versionName = "0.1.0"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
+
+	dependenciesInfo {
+		includeInApk = false
+		includeInBundle = false
 	}
 
 	signingConfigs {
